8a8ed55c6e98b6f0540bc8f75aa947bb64941cc1
Adam Mohammed <13.adamm@gmail.com>
2021-03-31 13:32:01 -0400

    added dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @products = Product.all #Setting the variables and will render in ERB template
+    @categories = Category.all
   end
 end
