2d420738e3933c6c26565d7f295a588958af8974
Adam Mohammed <13.adamm@gmail.com>
2021-04-07 16:49:23 -0400

    updated login bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,19 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    puts "test"
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
+      # puts user.errors.full_messages
       redirect_to '/signup'
     end
   end
 
     private
     def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
-
-
 
 end
